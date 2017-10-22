f10c2ea7969b7272c8af8aac1fb3f869b85edb55
violet <sakutacase@gmail.com>
2017-10-22 21:05:10 +0900

    koremo

--- src/test.js
+++ src/test.js
@@ -5,7 +5,7 @@
     request({
         url: 'http://www.nicovideo.jp/api/mylist/list',
         headers: {
-        Cookie: 'user_session=user_session_4443337_b38ccc7f67f762cd24187ee695768a413424c1d9d0470a7a01bff63ebebdb2e6;',
+        Cookie: '',
         },
     }, function(err, data){
         console.log(data);
